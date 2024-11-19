176a67cb539687fe228fbb7d12396f0b594285d7
Pratham Tamrakar <prathamtamrakar849gmail.com>
2024-11-19 12:20:48 +0530

    commited

--- lecture-4/api.js
+++ lecture-4/api.js
@@ -3,7 +3,19 @@ fetch('https://dummyjson.com/recipes').then((data)=>{
     return data.json()
     // console.log(data);
 }).then((a)=>{
-    console.log(a)
+    // console.log(a)
+    // console.log(a.recipes[1].name);
+
+    // a.recipes.map((value)=>{
+    // console.log(value.id);
+    // console.log(value.name);
+    // })
+
+    let data = a.recipes.filter((value)=>{
+        return value.cuisine=="Indian"
+    })
+    console.log(data);
+    
 }).catch((err)=>{
     console.log(err);
 })
